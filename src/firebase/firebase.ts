da21722a761c2fa5f1c6519a5c0c9d12bacfbaad
Mystypix <jakub.smagin@gmail.com>
2023-01-09 13:11:28 +0100

    firebase init

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,11 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
+
+// Initialize Firebase
+// initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth
+// const auth = getAuth();
+
+// export { auth };
