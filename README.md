e383267d1411a6fbc3087589fc03854bfe7abfb9
Mystypix <jakub.smagin@gmail.com>
2023-01-11 21:43:56 +0100

    added firebase config, doppler, sign up, sign in and logout methods, updated router

--- README.md
+++ README.md
@@ -8,6 +8,20 @@ Note: you can add alias to the terminal config (`.bashrc`, `.zshrc...`) like `al
 
 - `pnpm install`
 
+### Mac os
+
+- `brew install gnupg`
+- `brew install dopplerhq/cli/doppler`
+- `doppler login` (Ask admin to add you to the project)
+- `doppler setup` (Y)
+
+### Windows
+
+- `mkdir -p $HOME/bin`
+- `curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh | sh -s -- --install-path $HOME/bin`
+- `winpty doppler login` (Ask admin to add you to the project)
+- `doppler setup` (Y)
+
 ## How to run dev
 
-- `pnpm dev`
+- `pnpm run dev`
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,1 +1,33 @@
-export const Layout = () => <div>Layout</div>;
+import { useAuthSignOut, useAuthUser } from "@react-query-firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase/firebase";
+import css from "./layout.module.scss";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  const navigate = useNavigate();
+  const mutation = useAuthSignOut(auth);
+  const user = useAuthUser(["user"], auth);
+
+  const handleLogout = () => {
+    mutation.mutate();
+
+    if (mutation.isSuccess) {
+      navigate("/");
+    }
+  };
+
+  return (
+    <div className={css.wrapper}>
+      {user.data && (
+        <button className={css.logout} onClick={handleLogout}>
+          Logout
+        </button>
+      )}
+      <div>{children}</div>
+    </div>
+  );
+};
