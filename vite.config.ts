08214591f35b893d0a57c5525080bb80a8e813ca
Mystypix <jakub.smagin@gmail.com>
2023-01-17 11:21:48 +0100

    renamed files to make them consistent, added css config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      generateScopedName: "[name]_[local]_[hash:base64:5]",
+      localsConvention: "camelCase",
+    },
+  },
 });
