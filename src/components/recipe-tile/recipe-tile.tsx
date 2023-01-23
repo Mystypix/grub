3a6e4dda4dc75c812d38bcacf7495b75ff1bcfa0
Jakub Kares (private) <jakub.kares@gmail.com>
2023-01-23 23:30:20 +0100

    Interface, scss and component edits

--- src/components/recipe-tile/recipe-tile.tsx
+++ src/components/recipe-tile/recipe-tile.tsx
@@ -2,33 +2,25 @@ import React from "react";
 import css from "./recipe-tile.module.scss";
 import RecipeInterface from "../../interfaces/recipe";
 import { useNavigate } from "react-router-dom";
-// TODO JaKa import rating component
+import clsx from "clsx"; //not necessary at last
 // TODO JaKa import like button component
 
-interface RecipeTileInputs {
-  isFavorite: boolean; // TODO JaKa what if unregistered user? what if registered user? the icon should change according to the fact that user liked it or not. But unregistered user does not provide this info. Registered user has it in his database document (noSQL) - needed second call for the user records ?
-  recipeData: RecipeInterface;
-}
-export const RecipeTile = ({ isFavorite, recipeData }: RecipeTileInputs) => {
-  // TODO JaKa like will be replaced by a like button component
+export const RecipeTile = (recipe: RecipeInterface) => {
+  // TODO I want to pass the whole object inside the component. Component itself will take the data. Is this the right syntax ? I have to try it myself ...
   // TODO JaKa image from a database
-  // TODO JaKa rating will be replace by a rating component
+  // TODO img will be in binary format in db
   const navigate = useNavigate();
 
   return (
     <div
-      className={css["recipe-tile"]}
+      className={css.recipeTile}
       onClick={() => {
-        navigate(`/recipe/${recipeData.id}`);
+        navigate(`/recipe/${recipe.id}`);
       }}
     >
-      <button className={isFavorite ? css["add-to-favorite"] : ""}>Like</button>
-      <img src="food.webp" className={css["recipe-image"]} /> // TODO binary
-      format in db
-      <div className={css["recipe-short-info"]}>
-        <p className={css["recipe-header"]}>{recipeData.name}</p>
-        <div className="rating"> </div>
-      </div>
+      {recipe.isFavorite && <button className={css.isFavorite}></button>}
+      <img src="food.webp" className={css.recipePreview} />
+      <h3 className={css.recipeHeader}>{recipe.name}</h3>
     </div>
   );
 };
--- src/interfaces/recipe.tsx
+++ src/interfaces/recipe.tsx
@@ -1,5 +1,6 @@
 export default interface RecipeInterface {
   id: number;
+  isFavorite: boolean;
   name: string;
   previewImage: BinaryData;
   rating: number;
