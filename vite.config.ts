4a2b708a7d562bc6ac893a97e962fea21dd332e5
Mystypix <jakub.smagin@gmail.com>
2023-01-25 14:23:21 +0100

    maybe

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import createSvgSpritePlugin from "vite-plugin-svg-sprite";
+import dotenv from "dotenv-vault-core";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
