cfb2dc365da3d0e7cb4d40625390c582b8940ae1
Jakub Smagin <51135436+Mystypix@users.noreply.github.com>
2023-02-03 22:52:17 +0100

    forms refactoring - added react hooks form (#22)

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -6,42 +6,33 @@ interface InputProps {
   disabled?: boolean;
   label?: React.ReactNode;
   name: string;
-  onChange?: (s: React.ChangeEvent<HTMLInputElement>) => void; // will setState for a form everytime input is changed
-  placeholder?: string; // hint for a user
+  placeholder?: string;
+  register: any; // TODO - type properly
+  validation?: Record<string, unknown>;
   type?: string;
-  value: string;
 }
 
 export const Input = ({
   disabled,
   label,
   name,
-  onChange,
   placeholder,
+  register,
+  validation = {},
   type = "text",
-  value,
-}: InputProps) => {
-  function onChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
-    onChange && onChange(event);
-  }
-
-  return (
-    <div className={css.wrapper}>
-      {label && (
-        <label className={css.label} htmlFor={name}>
-          {label}
-        </label>
-      )}
-      <input
-        className={clsx(css.input, { [css.disabled]: disabled })}
-        disabled={disabled}
-        id={name}
-        name={name}
-        onChange={onChangeHandler}
-        placeholder={placeholder}
-        type={type}
-        value={value}
-      />
-    </div>
-  );
-};
+}: InputProps) => (
+  <div className={css.wrapper}>
+    {label && (
+      <label className={css.label} htmlFor={name}>
+        {label}
+      </label>
+    )}
+    <input
+      {...register(name, { ...validation })}
+      className={clsx(css.input, { [css.disabled]: disabled })}
+      disabled={disabled}
+      placeholder={placeholder}
+      type={type}
+    />
+  </div>
+);
